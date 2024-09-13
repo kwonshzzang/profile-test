240558d4be1c0119ddfa07bcf828c4cce92ada0c
kwonshzzang <kwonshzzang@gmail.com>
2024-09-13 18:22:22 +0900

    First Commits

---
+++ src/test/java/kr/co/kwonshzzang/profiletest/ProfileTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.profiletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
